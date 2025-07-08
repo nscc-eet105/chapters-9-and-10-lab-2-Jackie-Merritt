1e8dfbb69fcfe34643766e9ee215a99b946c3be4
Jackie Merritt <remmerritt@gmail.com>
2025-07-08 17:30:53 -0400

    Assignment Complete

--- Chp9-10_Lb2.py
+++ Chp9-10_Lb2.py
@@ -1,28 +1,39 @@
 #Jackie-Merritt_Chp9-10-Lab2_7/7/2025
+import sys
 
 def main():
     print('File Processor\n')
     file_name = input('Enter the name of the file you wish to process: ')
-    words = get_words(file_name)
-    print(f'The total number of words were {len(words)}')
-    print(words)
+    contents = open_file(file_name)
+    words = get_words(contents)
+    total_cap = get_capitals(contents)
+    print(f'The file {file_name} contains {len(words)} words of which {total_cap} of them are capitilized.')
 
-def get_words(file_name):
+def get_words(contents):
+        words = contents.split()
+        words.sort()
+        return words
+
+def get_capitals(contents):
+    words = contents.split()
+    capitilized = []
+    for num in words:
+        if num.islower() == False:
+            capitilized.append(num)
+    total_cap = len(capitilized)
+    return total_cap
+            
+
+def open_file(file_name):
     try:
         with open(file_name) as file:
             contents = file.read()
             file.close()
-        contents = contents.replace("\n", "")
-        contents = contents.replace(",", "")
-        contents = contents.replace(".", "")
-        contents = contents.replace("â€“", "")
-        contents = contents.lower()
-
-        words = contents.split(" ")
-        words.sort()
-        return words
-
+            return contents
+            
     except FileNotFoundError:
         print('File could not be found')
+        sys.exit()
+    
     
 main()
